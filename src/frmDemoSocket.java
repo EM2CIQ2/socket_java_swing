2bbe4071c0f61557dba6b21b38388ea2c519d032
Unero <20809454+Unero@users.noreply.github.com>
2020-09-28 15:02:42 +0700

    Initial commit

---
+++ src/frmDemoSocket.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class frmDemoSocket {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+    private JTextField txtPortServer;
+    private JTextField txtIPServer;
+    private JButton btnListen;
+    private JTextField txtPesanDiterimaServer;
+    private JTextField txtPortClient;
+    private JTextField txtAlamatIPTujuan;
+    private JTextField txtPesanDikirimKeServer;
+    private JButton btnKirim;
+}
