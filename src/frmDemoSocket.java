cb4cf708c369b7771a04b96451a9b0a63ed0c051
Unero <20809454+Unero@users.noreply.github.com>
2020-10-01 09:40:02 +0700

    Create Server Function

--- src/frmDemoSocket.java
+++ src/frmDemoSocket.java
@@ -2,7 +2,7 @@ import java.awt.event.ActionEvent;
 import javax.swing.*;
 import java.awt.event.ActionListener
 
-    public class frmDemoSocket {
+public class frmDemoSocket {
   private JTabbedPane tabbedPane1;
   private JPanel panel1;
   private JTextField txtPortServer;
@@ -125,4 +125,78 @@ import java.awt.event.ActionListener
           }
         });
   }
+
+    // Socket Server
+    public void EchoServer( String bindAddr, int bindPort ) throws IOException {
+        InetSocketAddress sockAddr = new InetSocketAddress(bindAddr, bindPort);
+
+        //create a socket channel and bind to local bind address
+        AsynchronousServerSocketChannel serverSock =  AsynchronousServerSocketChannel.open().bind(sockAddr);
+
+        //start to accept the connection from client
+        serverSock.accept(serverSock, new CompletionHandler<AsynchronousSocketChannel,AsynchronousServerSocketChannel >() {
+
+            @Override
+            public void completed(AsynchronousSocketChannel sockChannel, AsynchronousServerSocketChannel serverSock ) {
+                //a connection is accepted, start to accept next connection
+                serverSock.accept( serverSock, this );
+                //start to read message from the client
+                startRead( sockChannel );
+
+            }
+
+            @Override
+            public void failed(Throwable exc, AsynchronousServerSocketChannel serverSock) {
+                System.out.println( "fail to accept a connection");
+            }
+
+        });
+    }
+
+    private void startRead( AsynchronousSocketChannel sockChannel ) {
+        final ByteBuffer buf = ByteBuffer.allocate(2048);
+
+        //read message from client
+        sockChannel.read( buf, sockChannel, new CompletionHandler<Integer, AsynchronousSocketChannel >() {
+
+            /**
+             * some message is read from client, this callback will be called
+             */
+            @Override
+            public void completed(Integer result, AsynchronousSocketChannel channel  ) {
+                buf.flip();
+
+                // echo the message
+                startWrite( channel, buf );
+
+                //start to read next message again
+                startRead( channel );
+
+                // menampilkan string pesan ke textfield txtPesanDiterimaServer
+                txtPesanDiterimaServer.setText(new String( buf.array()));
+            }
+
+            @Override
+            public void failed(Throwable exc, AsynchronousSocketChannel channel ) {
+                System.out.println( "fail to read message from client");
+            }
+        });
+    }
+
+    private void startWrite( AsynchronousSocketChannel sockChannel, final ByteBuffer buf) {
+        sockChannel.write(buf, sockChannel, new CompletionHandler<Integer, AsynchronousSocketChannel >() {
+
+            @Override
+            public void completed(Integer result, AsynchronousSocketChannel channel) {
+                //finish to write message to client, nothing to do
+            }
+
+            @Override
+            public void failed(Throwable exc, AsynchronousSocketChannel channel) {
+                //fail to write message to client
+                System.out.println( "Fail to write message to client");
+            }
+
+        });
+    }
 }
